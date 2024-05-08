7c25cee6d19af62b4d5bb16229ae15226bddb1e0
VikasSahu1998 <sahuvikas1998@gmail.com>
2024-05-08 17:42:32 +0530

    Payment

--- src/app/Users/users-pricing-plans/users-pricing-plans.component.ts
+++ src/app/Users/users-pricing-plans/users-pricing-plans.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var Razorpay: any;
 @Component({
   selector: 'app-users-pricing-plans',
   templateUrl: './users-pricing-plans.component.html',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class UsersPricingPlansComponent {
 
+  constructor() { }
+
+  handlePayment(planName: string, planAmount: number) {
+    const RozarpayOptions = {
+      key: 'rzp_test_IScA4BP8ntHVNp',
+      amount: planAmount * 100,
+      currency: 'INR',
+      name: 'Cognitive Navigation Pvt. Ltd',
+      description: `${planName} Plan Subscription`,
+      image: 'https://imgur.com/a/J4UAMhv',
+
+      prefill: {
+        name: 'Vikas',
+        email: 'user@example.com',
+        contact: '9999999999'
+      },
+      theme: {
+        color: '#528FF0'
+      },
+      payment_method: {
+        external: ['upi']
+      }
+    };
+
+    const rzp = new Razorpay(RozarpayOptions);
+    rzp.open();
+
+  }
 }
